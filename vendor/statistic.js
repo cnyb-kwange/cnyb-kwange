0b6ab720a88f9e68e4763920bed47a28b14f996c
cnyb-kwange <648965988@qq.com>
2019-07-04 12:37:51 +0800

    commit

---
+++ vendor/statistic.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1882e1bbbe2e3568c5c9dad3e84f9fe9";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
