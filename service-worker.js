92247583f3b8d546db29719d6da557bea4c6d3cf
cnyb-kwange <648965988@qq.com>
2019-07-13 09:03:11 +0800

    commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.691af5f0d8106b0e4d51c0b38f362b51.js"
+  "/precache-manifest.1d836186a131fdf74a251eb6226e3793.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "crawler-client"});
